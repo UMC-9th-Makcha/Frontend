7542051ebd6dea5258fd8b844303474a42c93380
miyeon04 <miyeoni241@gmail.com>
2026-01-07 17:25:42 +0900

    ✨ feat: 알림 내역 모바일 UI 및 반응형 레이아웃 구현

--- Makcha/src/pages/History/components/EmptyHistoryCard.tsx
+++ Makcha/src/pages/History/components/EmptyHistoryCard.tsx
@@ -2,10 +2,20 @@ import LogoIcon from "../../../assets/icons/logo.svg";
 
 const EmptyHistoryCard = () => {
     return (
-        <div className="w-full rounded-[20px] border border-gray-200 bg-white shadow-sm">
-            <div className="flex flex-col items-center pt-6.25 pb-4.75">
-                <img src={LogoIcon} alt="logo" className="h-22 w-22" />
-                <p className="mt-4 text-[16px] text-[#5F5F5F]">
+        <div
+            className="
+                w-full rounded-[20px]
+                border border-gray-200 bg-white shadow-sm
+                dark:border-makcha-navy-700 dark:bg-makcha-navy-900
+            "
+        >
+            <div className="flex flex-col items-center pt-[25px] pb-[19px]">
+                <img
+                    src={LogoIcon}
+                    alt="logo"
+                    className="h-22 w-22 opacity-90 dark:opacity-100"
+                />
+                <p className="mt-4 text-[16px] text-gray-500 dark:text-white">
                     이용 내역이 없습니다.
                 </p>
             </div>
