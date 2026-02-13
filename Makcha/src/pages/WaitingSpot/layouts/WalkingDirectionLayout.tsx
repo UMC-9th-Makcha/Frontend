88e2bc9ad53914394552a6cdcddeadc733668409
inyouxg <dolley03@naver.com>
2026-02-13 19:17:43 +0900

    ✨ feat: 도보 길안내 모바일뷰 바텀시트 추가

--- Makcha/src/pages/WaitingSpot/layouts/WalkingDirectionLayout.tsx
+++ Makcha/src/pages/WaitingSpot/layouts/WalkingDirectionLayout.tsx
@@ -1,8 +1,9 @@
 import type { WaitingSpotLayoutProps } from "../types/waitingspot";
 import Panel from "../../../components/common/Panel";
 import SubPanel from "../../../components/common/Panel/SubPanel";
+import { MobileBottomSheet } from "../common/MobileBottomSheet";
 
-export const WalkingDirectionLayout = ({header, controls, map, search, list, detail, footer, onDetailBack}: WaitingSpotLayoutProps) => {
+export const WalkingDirectionLayout = ({ header, controls, map, search, list, detail, footer, onDetailBack }: WaitingSpotLayoutProps) => {
   return (
     <div className="relative h-dvh w-full">
       {/* map */}
@@ -14,12 +15,12 @@ export const WalkingDirectionLayout = ({header, controls, map, search, list, det
         <Panel
           width="md:w-100"
           isMobileFull={true}
-          className="relative inset-x-0 z-10 bg-white pointer-events-auto dark:bg-makcha-navy-900 md:relative md:top-0 md:h-dvh md:shadow-[5px_0px_15px_0px_#88888859]"
+          className={`pointer-events-auto h-full ${detail ? "hidden md:flex" : "md:flex"}`}
           disablePadding
           contentClassName="h-full min-h-0 flex flex-col"
         >
-          <div 
-          className="flex-1 min-h-0 overflow-y-auto px-[16px] pt-[40px] pb-6">
+          <div
+            className="flex-1 min-h-0 overflow-y-auto px-[16px] pt-[40px] pb-6">
             {header}
             {search}
             {controls}
@@ -27,7 +28,7 @@ export const WalkingDirectionLayout = ({header, controls, map, search, list, det
           </div>
 
           {footer && (
-            <div className="shrink-0 px-[16px] pb-20">
+            <div className="shrink-0 px-[16px]">
               {footer}
             </div>
           )}
@@ -38,11 +39,19 @@ export const WalkingDirectionLayout = ({header, controls, map, search, list, det
           onBack={onDetailBack}
           title="경로 안내"
           width="md:w-100"
-          className="pointer-events-auto"
+          className="hidden md:flex pointer-events-auto"
         >
           {detail}
         </SubPanel>
+
       </div>
+      { detail && (
+        <div className={`md:hidden absolute inset-0 z-20 pointer-events-none`}>
+          <MobileBottomSheet title="도보 길 안내">
+            {detail}
+          </MobileBottomSheet>
+        </div>
+      )}
     </div>
   );
 };
--- Makcha/src/pages/WaitingSpot/panels/DirectionDetailPanel.tsx
+++ Makcha/src/pages/WaitingSpot/panels/DirectionDetailPanel.tsx
@@ -12,21 +12,23 @@ export const DirectionDetailPanel = ({ direction, routeDetail }: DirectionDetail
     <div className="flex flex-col h-full">
       <div className="pb-4">
         <div className="font-medium text-base leading-[1.2] text-makcha-navy-600 pb-1">
-        {getRouteCategoryLabel(direction.summary.category)}
-      </div>
-      <div className="flex gap-3">
-        <div className="font-medium text-[32px] leading-[1.2] text-[#262626]">
-          {Math.ceil(direction.summary.durationSeconds / 60)}분
+          {getRouteCategoryLabel(direction.summary.category)}
         </div>
-        <div className="font-normal text-[32px] leading-[1.2] text-[#262626]">
-          {direction.summary.distanceMeters}m
+        <div className="flex gap-3">
+          <div className="font-medium text-[32px] leading-[1.2] text-[#262626]">
+            {Math.ceil(direction.summary.durationSeconds / 60)}분
+          </div>
+          <div className="font-normal text-[32px] leading-[1.2] text-[#262626]">
+            {direction.summary.distanceMeters}m
+          </div>
         </div>
-      </div>
-      <div className="font-normal text-[16px] leading-[1.2] text-[#262626]">
-        횡단보도 {direction.summary.crosswalkCount}회
-      </div>
+        <div className="font-normal text-[16px] leading-[1.2] text-[#262626]">
+          횡단보도 {direction.summary.crosswalkCount}회
+        </div>
       </div>
       <hr className="text-[#E2E2E2] -mx-4" />
+
+      {/* 스크롤 영역 */}
       <div className="flex flex-col gap-2 mt-4">
         {steps.map((step) => (
           <DirectionDetailCard key={step.order} step={step} />
